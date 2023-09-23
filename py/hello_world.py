867b8edc9e9d2ac2336db59bb026e99183bbb8eb
api2gether <134139475+api2gether@users.noreply.github.com>
2023-09-23 11:57:57 +0200

    HelloWorld v2.0

--- py/hello_world.py
+++ py/hello_world.py
@@ -1,5 +1,5 @@
 # -*- coding: utf8 -*-
-""" HelloWorld script using standard pythonpart
+"""HelloWorld script using standard pythonpart
 
 Author:
     Christophe MAIGNAN @ API2GETHER - 2023
@@ -14,12 +14,19 @@ from BuildingElement     import BuildingElement
 from CreateElementResult import CreateElementResult
 from PythonPartUtil      import PythonPartUtil
 
-print('Load HelloWorld.py')
+from HandlePropertiesService import HandlePropertiesService
+from HandleDirection         import HandleDirection
+from HandleParameterData     import HandleParameterData
+from HandleParameterType     import HandleParameterType
+from HandleProperties        import HandleProperties
+
+
+print('Load HelloWorld')
 
 
 def check_allplan_version(build_ele : BuildingElement,
                           version   : float) -> bool:
-    """ Check the current Allplan version
+    """Check the current Allplan version
 
     Args:
         build_ele : the building element
@@ -28,14 +35,33 @@ def check_allplan_version(build_ele : BuildingElement,
     Returns:
         True/False if version is supported by this script
     """
-
     # Support all versions
     return True
 
 
+def move_handle(build_ele   : BuildingElement,
+                handle_prop : HandleProperties,
+                input_pnt   : Geometry.Point3D,
+                doc         : ElementAdapter.DocumentAdapter) -> CreateElementResult:
+    """Called after modification of the element geometry using handles
+
+    Args:
+        build_ele   : building element with the parameter properties
+        handle_prop : handle properties
+        input_pnt   : input point
+        doc         : input document
+
+    Returns:
+        Object with the result data of the element creation
+    """
+    HandlePropertiesService.update_property_value(build_ele, handle_prop, input_pnt)
+
+    return create_element(build_ele, doc)
+
+
 def create_element(build_ele : BuildingElement,
                    doc       : ElementAdapter.DocumentAdapter) -> CreateElementResult:
-    """ Creation of element
+    """Creation of element
 
     Args:
         build_ele : the building element
@@ -45,15 +71,19 @@ def create_element(build_ele : BuildingElement,
         result of the created element
     """
     model_ele_list = []
+    handle_list    = []
 
     pyp_util = PythonPartUtil()
+
+    # Extract parameters values ​​from palette
+    line_length = build_ele.LineLength.value
 
     # Define common properties
     com_prop = BaseElements.CommonProperties()
     com_prop.GetGlobalProperties()
 
     # Create 2D line
-    line = Geometry.Line2D(0, 0, 1000, 0)
+    line = Geometry.Line2D(0, 0, line_length, 0)
 
     # Add line to 2d view
     pyp_util.add_pythonpart_view_2d(BasisElements.ModelElement2D(com_prop, line))
@@ -61,4 +91,15 @@ def create_element(build_ele : BuildingElement,
     # Create the PythonPart
     model_ele_list = pyp_util.create_pythonpart(build_ele)
 
-    return CreateElementResult(model_ele_list)
+    # Create the handles
+    handle_length = HandleProperties("LineLengthHandle",
+                                     Geometry.Point3D(line_length, 0, 0),
+                                     Geometry.Point3D(),
+                                     [HandleParameterData("LineLength", HandleParameterType.POINT_DISTANCE)],
+                                     HandleDirection.X_DIR
+                                     )
+
+    handle_length.info_text = "Longueur"
+    handle_list.append(handle_length)
+
+    return CreateElementResult(model_ele_list, handle_list)
--- pyp/HelloWorld.pyp
+++ pyp/HelloWorld.pyp
@@ -5,7 +5,21 @@
     <Script>
         <Name>APIHub\hello_world.py</Name>
         <Title>HelloWorld</Title>
-        <Version>1.0</Version>
+        <Version>2.0</Version>
     </Script>
+
+    <Page>
+
+        <Name>Page1</Name>
+        <Text>Général</Text>
+
+        <Parameter>
+            <Name>LineLength</Name>
+            <Text>Longueur</Text>
+            <Value>1000.0</Value>
+            <ValueType>Length</ValueType>
+        </Parameter>
+
+    </Page>
 
 </Element>
